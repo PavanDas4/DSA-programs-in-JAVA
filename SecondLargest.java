445b07157c5eb2d86993611ea2ac6bfccd843a5d
Pavan Das <pavandas1029@gmail.com>
2024-11-20 21:01:26 +0530

    GFG

---
+++ SecondLargest.java
@@ -0,0 +1,27 @@
+/*
+ * Find the second largest element in an array
+ * Input: {3, 2, 1, 5, 5, 4}
+ * Output: 4
+ */
+
+import java.util.Arrays;
+
+public class SecondLargest {
+  public static int sol(int[] arr) {
+    Arrays.sort(arr); // sort the array
+    int largest = arr[arr.length - 1]; // get the largest element
+    int secondLargest = -1; // initialize the second largest element
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] < largest && arr[i] != largest) {
+        // check if the current element is smaller than the largest element and not equal
+        secondLargest = arr[i]; // update the second largest element
+      }
+    }
+    return secondLargest;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 3, 2, 1, 5, 5, 4 };
+    System.out.println(sol(arr));
+  }
+}
