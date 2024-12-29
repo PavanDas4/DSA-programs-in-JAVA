2e5eaa7d5e676fb2f1f02a74b4bc3c8d0a6f1bfa
Pavan Das <pavandas1029@gmail.com>
2024-12-29 21:00:19 +0530

    matrix zeros

--- Array/Set matrix zeros/solution.java
+++ Array/Set matrix zeros/solution.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class solution {
   public static int[][] SetZero(int[][] mat, int[] n) {
+    boolean frow = false, fcol = false;
+    for (int i = 0; i < n[1]; i++) {
+      if (mat[0][i] == 0) {
+        frow = true;
+        break;
+      }
+    }
     for (int i = 0; i < n[0]; i++) {
-      for (int j = 0; j < n[1]; j++) {
+      if (mat[i][0] == 0) {
+        fcol = true;
+        break;
+      }
+    }
+    for (int i = 1; i < n[0]; i++) {
+      for (int j = 1; j < n[1]; j++) {
         if (mat[i][j] == 0) {
           mat[0][j] = 0;
           mat[i][0] = 0;
@@ -11,22 +24,20 @@ public class solution {
       }
     }
     for (int i = 1; i < n[0]; i++) {
-      if (mat[i][0] == 0) {
-        for (int j = 0; j < n[1]; j++) {
+      for (int j = 1; j < n[1]; j++) {
+        if (mat[i][0] == 0 || mat[0][j] == 0) {
           mat[i][j] = 0;
         }
       }
     }
-    for (int j = 1; j < n[1]; j++) {
-      if (mat[0][j] == 0) {
-        for (int i = 0; i < n[0]; i++) {
-          mat[i][j] = 0;
-        }
+    if (frow) {
+      for (int i = 0; i < n[1]; i++) {
+        mat[0][i] = 0;
       }
     }
-    if (mat[0][0] == 0) {
-      for (int j = 0; j < n[1]; j++) {
-        mat[0][j] = 0;
+    if (fcol) {
+      for (int i = 0; i < n[0]; i++) {
+        mat[i][0] = 0;
       }
     }
     return mat;
@@ -39,7 +50,7 @@ public class solution {
     n[0] = sc.nextInt();
     n[1] = sc.nextInt();
     int[][] mat = new int[n[0]][n[1]];
-    System.out.println("Enter the elements of the matrix: ");
+    System.out.println("Enter the elements of the mat: ");
     for (int i = 0; i < n[0]; i++) {
       for (int j = 0; j < n[1]; j++) {
         mat[i][j] = sc.nextInt();
@@ -47,7 +58,7 @@ public class solution {
     }
     sc.close();
     SetZero(mat, n);
-    System.out.println("The matrix after setting zeros is: ");
+    System.out.println("The mat after setting zeros is: ");
     for (int i = 0; i < n[0]; i++) {
       for (int j = 0; j < n[1]; j++) {
         System.out.print(mat[i][j] + " ");
