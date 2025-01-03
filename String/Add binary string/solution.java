666b9a1030a766a2178b227209ba3f051b2405f6
Pavan Das <pavandas1029@gmail.com>
2025-01-03 21:32:27 +0530

    add binary string

--- String/Add binary string/solution.java
+++ String/Add binary string/solution.java
@@ -1,13 +1,27 @@
-import java.math.BigInteger;
+// Add Binary Strings
 import java.util.Scanner;
 
 public class solution {
   public static String addBinary(String a, String b) {
-    BigInteger num1, num2;
-    num1 = new BigInteger(a, 2);
-    num2 = new BigInteger(b, 2);
-    num1.add(num2);
-    return num1.toString(2);
+    StringBuilder result = new StringBuilder();
+    int i = a.length() - 1, j = b.length() - 1, carry = 0;
+    while (i >= 0 || j >= 0) {
+      int sum = carry;
+      if (i >= 0)
+        sum += a.charAt(i--) - '0';
+      if (j >= 0)
+        sum += b.charAt(j--) - '0';
+      result.append(sum % 2);
+      carry = sum / 2;
+    }
+    if (carry != 0)
+      result.append(carry);
+    String finalResult = result.reverse().toString();
+    int index = 0;
+    while (index < finalResult.length() && finalResult.charAt(index) == '0') {
+      index++;
+    }
+    return index == finalResult.length() ? "0" : finalResult.substring(index);
   }
 
   public static void main(String[] args) {
